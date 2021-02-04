03d45fa7c1bf6cc9e00bbfb82ad1ed01dbc5344f
Troy Lemons Jr <troylemonsjr@gmail.com>
2021-02-04 12:36:25 -0800

    got controllers with CRUD setup

--- controllers/tunr.js
+++ controllers/tunr.js
@@ -2,12 +2,27 @@ const tunr = require("../models/tunr")
 const {Router} = require("express")
 const router = Router()
 
+
 //index route
-
+router.get('/', async(req,res) => {
+    const allSongs = await tunr.find({})
+    res.json({status: 200, data: allSongs})
+})
 //create route
-
+router.post('/', async (req, res) => {
+    res.json(await tunr.create(req.body))
+})
 //update route
-
+router.put('/:_id', (req,res) => {
+    tunr.findByIdAndUpdate(req.params._id, req.body).then((song) => {
+        res.json(song)
+    })
+})
 //delete route
+router.delete("/:_id", (req,res) => {
+    tunr.findByIdAndDelete(req.params._id, req.body).then((song) => {
+        res.json(song)
+    })
+})
 
 module.exports = router
